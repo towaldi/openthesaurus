05c687110d31ed0b1512c9464013924d238c9ae1
towaldi <tobiaswalddoerfer@gmail.com>
2023-03-17 12:24:53 +0100

    API successfully addressed

---
+++ script.js
@@ -0,0 +1,6 @@
+async function getSynonyms() {
+    let url = 'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
+    let response = await fetch(url);
+    let responseAsJson = await response.json()
+    console.log('Response is', responseAsJson);
+}
