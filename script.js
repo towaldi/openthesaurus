4b6b1b758aa1d0c2f1da7db98077474948ee2b58
towaldi <tobiaswalddoerfer@gmail.com>
2023-03-19 17:38:06 +0100

    show synonyms which have been loaded

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 async function getSynonyms() {
     let url = 'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
     let response = await fetch(url);
-    let responseAsJson = await response.json()
-    console.log('Response is', responseAsJson);
+    let responseAsJson = await response.json();
+
+    let synsets = responseAsJson['synsets'];
+    renderSynsets(synsets);
+    // console.log('Response is', responseAsJson);
+}
+
+// Adds synsets to body (html)
+function renderSynsets(synsets) {
+    let container = document.getElementById('container');
+
+    container.innerHTML = /*html*/  `<div>
+        <p><b>${synsets.length}</b>synonyms have been loaded</p>
+    </div>`;
 }
